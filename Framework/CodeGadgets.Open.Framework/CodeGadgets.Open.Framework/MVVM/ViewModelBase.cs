4c2fa0768658a684dce62ecbb266f4b0d7efdf1d
kevinferno <kevinferno@gmail.com>
2015-07-30 15:04:41 -0400

    Removed name property from ViewModelBase ViewModelBase is now just a default implementation of INotifyPropertyChanged

--- Framework/CodeGadgets.Open.Framework/CodeGadgets.Open.Framework/MVVM/ViewModelBase.cs
+++ Framework/CodeGadgets.Open.Framework/CodeGadgets.Open.Framework/MVVM/ViewModelBase.cs
@@ -10,18 +10,6 @@ namespace CodeGadgets.Open.Framework.MVVM
 	public class ViewModelBase : INotifyPropertyChanged
 	{
 		public event PropertyChangedEventHandler PropertyChanged;
-		public string Name
-		{
-			get { return _Name; }
-			set
-			{
-				if (_Name == value) return;
-				_Name = value;
-				OnPropertyChanged(nameof(Name));
-			}
-		}
 		protected virtual void OnPropertyChanged(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-
-		private string _Name;
 	}
 }
